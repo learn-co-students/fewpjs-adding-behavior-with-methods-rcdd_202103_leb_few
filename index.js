8700fc304056411c91ee3ca747703c1fdaa34df6
Amro dandashli <amrdandashli@gmail.com>
2021-05-08 00:33:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+
+
+class Cat {
+constructor (name,sex) {
+
+  this.name= name;
+  this.sex= sex;
+}
+speak() {
+  return  `${this.name} says meow!`
+}
+}
+
+let animal = new Cat('ddd')
+
+class Dog {
+  constructor (name,sex) {
+
+    this.name= name;
+    this.sex= sex;
+  }
+speak(){
+  return `${this.name} says woof!`
+}
+}
+let newdog = new Dog('asdasd')
+
+class Bird {
+  constructor (name,sex) {
+
+    this.name= name;
+    this.sex= sex;
+  }
+  speak(){ if(this.sex==='male'){
+    return `It's me! ${this.name}, the parrot!`}else{
+      return `${this.name} says squawk!`
+    }
+
+  }
+}
+let newbird = new Bird('Mable')
